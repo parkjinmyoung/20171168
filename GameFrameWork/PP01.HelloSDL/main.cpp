8b40cdb0e07df1c665cc9cef0f4d257dc634e58f
user <user@532-28>
2018-11-08 18:09:41 +0900

    PP02.BreakUpHelloSDL

--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -37,7 +37,7 @@ void render()
 
 int main(int argc, char* argv[])
 {
-	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
+	if (init("Chapter 2 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
 		g_bRunning = true;
 	}
 	else {

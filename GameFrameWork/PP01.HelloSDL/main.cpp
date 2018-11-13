9d2e43a102cabff125ee0e6568cd7bf26cc6cd07
user <user@532-28>
2018-11-13 12:47:46 +0900

    pp03.GameClass

--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -1,53 +1,24 @@
 #include<SDL.h>
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenterer = 0;
+#include "Game.h"
 
 
+Game* g_gmae = 0;
 
-
-bool g_bRunning = false;
-
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-		if (g_pWindow != 0)
-		{
-			g_pRenterer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else {
-		return false;
-	}
-	return true;
-
-}
-
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenterer, 0, 0, 0, 255);
-
-	SDL_RenderClear(g_pRenterer);
-
-	SDL_RenderPresent(g_pRenterer);
-}
 
 int main(int argc, char* argv[])
 {
-	if (init("Chapter 2 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
-		g_bRunning = true;
-	}
-	else {
-		return 1;
-	}
-	while (g_bRunning)
-	{
-		render();
-	}
-	SDL_Quit();
+	g_gmae = new Game();
+	g_gmae->init("Chapter3", 100, 100, 640, 480, false);
 
+	while (g_gmae->running())
+	{
+		g_gmae->handleEvents();
+		g_gmae->update();
+		g_gmae->render();
+
+	}
+
+	g_gmae->clean();
 	return 0;
 }

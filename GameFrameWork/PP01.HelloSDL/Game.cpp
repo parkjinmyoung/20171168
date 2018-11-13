500a38541e4bd3f4de361e4af1b1f5d7f74ef249
user <user@532-28>
2018-11-13 12:52:53 +0900

    PP04.BasicDrawing

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -11,6 +11,19 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
 
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+
+		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+
+
+
+
 		m_bRunning = true;
 	}
 	else
@@ -24,7 +37,13 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 void Game::render()
 {
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+
 	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
 }
 
